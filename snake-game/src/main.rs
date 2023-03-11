e3d643a5d9e9a5a548b9027528403d70b7a71e7b
murshidazher <hello@murshidazher.com>
2023-03-11 17:21:49 +0530

    feat: add new function

--- snake-game/src/main.rs
+++ snake-game/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+  let message = "Hello World";
+  let message_2 = print_welcome(message);
+  // println is a macro and they end with an ! (exclamation)
+  println!("{}", message_2);
+}
+
+fn print_welcome(text: &str) -> &str {
+  println!("{}", text);
+  let new_message = "Hi There";
+  // return new_message;
+  new_message // we don't need to explicitly write the return keyword to return. A value ending without semi-colon would be returned
 }
