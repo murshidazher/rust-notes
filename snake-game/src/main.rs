2d1fad3d501a9efd03c0871124619ec600dd5734
murshidazher <hello@murshidazher.com>
2023-03-12 02:51:24 +0530

    feat: string move

--- snake-game/src/main.rs
+++ snake-game/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
   // you can place on stack only values with static size
   let a = 10;
-  let b = a;
+  let b = a; // copying
   let c = 15;
   let d = add(a, b);
 
@@ -13,7 +13,11 @@ fn main() {
   // the pointer (address) to the heap memory will be saved in stack
   // ptr -> capacity (size in memory) -> length (actual length of string)
   let message = String::from("Hello");
-  println!("{}", message);
+  let message_2 = message;
+
+  //  cannot use message because it was moved to message_2
+  // println!("{}", message);
+  println!("{}", message_2);
 }
 
 fn add(x: u32, y: u32) -> u32 {
