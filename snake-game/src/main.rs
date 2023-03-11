56e14d4a9059a64f480bb49ac27b475c65508081
murshidazher <hello@murshidazher.com>
2023-03-11 17:31:51 +0530

    feat: primitive types 1

--- snake-game/src/main.rs
+++ snake-game/src/main.rs
@@ -1,13 +1,21 @@
 fn main() {
-  let message = "Hello World";
-  let message_2 = print_welcome(message);
-  // println is a macro and they end with an ! (exclamation)
-  println!("{}", message_2);
-}
+  let is_it_fun = false;
+  // i32 -> signed integer of 32bits -> 2^31 – 1 (max number)
+  // signed can hold positive and negative values
+  let num = -10;
 
-fn print_welcome(text: &str) -> &str {
-  println!("{}", text);
-  let new_message = "Hi There";
-  // return new_message;
-  new_message // we don't need to explicitly write the return keyword to return. A value ending without semi-colon would be returned
+  // u32 -> 2^32 - 1
+  // u8 -> unsigned integer of 8bits
+  // 2^8 - 1 = 255
+  let small_num: u8 = 255;
+
+  // -2^7 -> 2^7 - 1
+  // -128 -> 127
+  let small_num_2: i8 = 127;
+
+  // architecture related types
+  // operating system related type
+  // isize or usize depends on arch will hold either 32bit or 64bit
+  let sys_num: isize = -10;
+  let sys_num_2: usize = 10;
 }
