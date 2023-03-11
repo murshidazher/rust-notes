8bc6ecd2dc83ee679d5f23210b9fa6bc8a4399ec
murshidazher <hello@murshidazher.com>
2023-03-12 03:06:45 +0530

    feat: return moved value

--- snake-game/src/main.rs
+++ snake-game/src/main.rs
@@ -1,22 +1,13 @@
 fn main() {
-  let message = String::from("Hello"); // message coming into scope
-  print_message(message); // message is moved to the print_message function
-  // message is no longer valid
+  let message = String::from("Hello");
+  let message_2 =extend_message(message);
 
-  // error: value borrowed here after move
-  // this is because the value is moved to c and then removed
-  // after the popping of print_message's stack frame
-  // println!("{}", message);
+  println!("{}", message_2);
 }
 
-fn print_message(a: String) { // a comes into the scope
-  println!("{}", a);
-  let c = a; // c is coming into scope and a is moved
-  // a is no longer valid
-
-  // this will also throw an error since a is moved
-  // println!("{}", a);
+// mark it as mutable string to append the world
+fn extend_message(mut a: String) -> String {
+  a.push_str(" World"); // we need to show a hello world message
+  a
 }
 
-// a is going out of scope, but nothing more will happen since it was moved
-// c is going out of scope and 'drop' is called which clears the underlying memory from the heap
