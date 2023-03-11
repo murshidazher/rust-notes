819b51186e06718ab15bd3f75a7c821dac77497c
murshidazher <hello@murshidazher.com>
2023-03-12 03:19:35 +0530

    feat: mutable borrow

--- snake-game/src/main.rs
+++ snake-game/src/main.rs
@@ -1,13 +1,16 @@
 fn main() {
-  let message = String::from("Hello");
-  let message_2: &String = &message;
+  let mut message = String::from("Hello"); // immutable borrow
+  let message_2: &mut String = &mut message; // mutable reference
+  // mutable reference will change both the pointers
   // message_2 is not owner of data
   // message_2 is "borrowing" a reference to message
   // message    |ptr|cap|len|
   // message_2  |ptr|
 
-  println!("{}", message);
+  message_2.push_str(" World");
+
   println!("{}", message_2);
+  println!("{}", message);
 }
 
 // message and message_2 going out of the scope
