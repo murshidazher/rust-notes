40ced0e4c3fb95c863ec1b2323abe5321d6a3b00
murshidazher <hello@murshidazher.com>
2023-03-11 17:43:05 +0530

    docs: add `utf8` charttable link

--- snake-game/src/main.rs
+++ snake-game/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
   let hex_num = 0xfa; // 250
   let bin_num = 0b0010_1011; // 43
 
-  let byte_num = b'A'; // 65
+  let byte_num = b'A'; // Ox41 -> 65 https://www.utf8-chartable.de/
 
   println!("{}", custom_num);
   println!("{}", hex_num);
