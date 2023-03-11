76fd44a0f40eed24e11bc9301443f560167a5eb4
murshidazher <hello@murshidazher.com>
2023-03-12 03:25:00 +0530

    feat: mutable borrow order matters

--- snake-game/src/main.rs
+++ snake-game/src/main.rs
@@ -1,18 +1,21 @@
 fn main() {
   let mut message = String::from("Hello"); // immutable borrow
-  let message_2: &mut String = &mut message; // mutable reference
-  // mutable reference will change both the pointers
-  // message_2 is not owner of data
-  // message_2 is "borrowing" a reference to message
-  // message    |ptr|cap|len|
-  // message_2  |ptr|
+  let message_3 = &message;
+  println!("{}", message_3);
+  let message_2 = &mut message;
+  // let message_4 = &message;
+  // we can't do this too since the is a mutate call above
 
-  message_2.push_str(" World");
+  // rust compiler doesn't allow the immutable to be print before mutable
+  // error -> cannot borrow `message` as immutable because it is also borrowed as mutable
+  // println!("{}", message);
+  // it's because this function can be async and can update the value of message while the program is running. This is unpredictable variability.
+  // unpredictable_mutate(message_2);
 
-  println!("{}", message_2);
+  unpredictable_mutate(message_2);
   println!("{}", message);
 }
 
-// message and message_2 going out of the scope
-// message_2 is not dropped because it doesn't have ownership of what it refers to
-// message is dropped
+fn unpredictable_mutate(val: &mut String) {
+  val.push_str("_unpredictable");
+}
