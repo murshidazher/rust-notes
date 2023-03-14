bb5fbf2645324d785948dca58c979643398f55b0
murshidazher <hello@murshidazher.com>
2023-03-14 23:59:44 +0530

    feat: de-referencing 3

--- snake-game/src/main.rs
+++ snake-game/src/main.rs
@@ -2,31 +2,24 @@
 // * means de-referencing
 // only by de-referencing a number can you get the value
 fn main() {
-  let mut message = String::from("Hello"); // immutable borrow
-  let message_2 = &mut message;
-
-  (*message_2).push_str(" World");
-  // same as
-  // message_2.push_str(" World");
-
-  println!("{}", message_2);
-
   let a = 10;
   let b = &a;
-  let c = &b;
-
-  println!("{}", a == **c);
-
   let mut c = &b;
   let d = b;
 
   let e = &&100;
-
   c = e;
 
-  // we can print the pointer address instead of the value
-  println!("e: {:p}", e);
-  println!("e: {:p}", *e);
-  println!("c: {:p}", c);
-  println!("c: {:p}", *c);
+  println!("Value of c: {:p}", c);
+  println!("Value of e: {:p}", e);
+  println!("Address of 100: {:p}", &(**e));
+
+  println!("Value of c: {:p}", *c);
+  println!("Value of e: {:p}", *e);
+  println!("Address of 100: {:p}", &(**c));
+
+  // println!("Address of a: {:p}", &a);
+  // println!("Value of b: {:p}", b);
+  // println!("Value of c: {:p}", c);
+  // println!("Value of d: {:p}", d);
 }
