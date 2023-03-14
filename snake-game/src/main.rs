e0fc6c7c3c0b53ad91f94323d0f3acacee6b7774
murshidazher <hello@murshidazher.com>
2023-03-15 00:04:12 +0530

    feat: `String` vs `&str`

--- snake-game/src/main.rs
+++ snake-game/src/main.rs
@@ -1,25 +1,9 @@
-// De-referencing
-// * means de-referencing
-// only by de-referencing a number can you get the value
+
 fn main() {
-  let a = 10;
-  let b = &a;
-  let mut c = &b;
-  let d = b;
+  let mut message = String::from("Hello");
+  let name = "Filip";
 
-  let e = &&100;
-  c = e;
-
-  println!("Value of c: {:p}", c);
-  println!("Value of e: {:p}", e);
-  println!("Address of 100: {:p}", &(**e));
-
-  println!("Value of c: {:p}", *c);
-  println!("Value of e: {:p}", *e);
-  println!("Address of 100: {:p}", &(**c));
-
-  // println!("Address of a: {:p}", &a);
-  // println!("Value of b: {:p}", b);
-  // println!("Value of c: {:p}", c);
-  // println!("Value of d: {:p}", d);
+  message.push_str(" World");
+  // name.push_str(" Jerga");
+  // String is a stack pointer while &str is a pointer to read-only memory in heap
 }
