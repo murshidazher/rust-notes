4b4b4d785b7f05055c6b3cbedf6f842e1d03d11f
murshidazher <hello@murshidazher.com>
2023-07-08 20:22:35 +0530

    feat: add modularity use

--- modularity/src/main.rs
+++ modularity/src/main.rs
@@ -1,3 +1,14 @@
+use modularity::Person;
+use modularity::Animal;
+use modularity::log_info;
+use modularity::log_info_2;
+
 fn main() {
-    println!("Hello, world!");
+    let mut person = Person::new();
+    let animal = Animal(String::from("dog"));
+
+    person.change_age(38);
+
+    log_info(person);
+    log_info_2(&animal);
 }
