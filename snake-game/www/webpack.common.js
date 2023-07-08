ed8e519fb1c0739d8027f94e2215cfa995615225
murshidazher <hello@murshidazher.com>
2023-07-09 04:13:18 +0530

    feat: convert to typescript

--- snake-game/www/webpack.common.js
+++ snake-game/www/webpack.common.js
@@ -8,6 +8,18 @@ module.exports = {
     path: path.resolve(__dirname, "public"),
     filename: "bootstrap.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
   plugins: [
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: ["public/*.wasm"],
