70cac5ee7d123440d5b4e2df11e2c8d8ba2d2b35
murshidazher <hello@murshidazher.com>
2023-07-09 05:34:18 +0530

    feat: add change direction guard clause

--- snake-game/src/lib.rs
+++ snake-game/src/lib.rs
@@ -62,6 +62,14 @@ impl World {
     }
 
     pub fn change_snake_dir(&mut self, direction: Direction) {
+        // guard to cover the edge cases of changing directing in the same lane
+        let next_cell = self.gen_next_snake_cell(&direction);
+
+        // if the change direction cell is the next cell, don't do any action
+        if self.snake.body[1].0 == next_cell.0 {
+            return;
+        }
+
         self.snake.direction = direction;
     }
 
@@ -75,7 +83,7 @@ impl World {
 
     pub fn step(&mut self) {
         let temp = self.snake.body.clone();
-        let next_cell = self.gen_next_snake_cell();
+        let next_cell = self.gen_next_snake_cell(&self.snake.direction);
         self.snake.body[0] = next_cell;
 
         // move the next snake body cell by the previous cell
@@ -86,11 +94,11 @@ impl World {
         }
     }
 
-    fn gen_next_snake_cell(&self) -> SnakeCell {
+    fn gen_next_snake_cell(&self, direction: &Direction) -> SnakeCell {
         let snake_idx = self.snake_head_idx();
         let row = snake_idx / self.width;
 
-        return match self.snake.direction {
+        return match direction {
             Direction::Right => {
                 let threshold = (row + 1) * self.width;
                 if snake_idx + 1 == threshold {
